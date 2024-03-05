8699f8c2381f0b6811959d7878f3c7cfe71a84fb
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-03-05 16:48:15 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello, World! in PHP</title>
+  </head>
+  <body>
+    <h1><?php echo 'Hello, World!'; ?></h1>
+    <?php echo '<p>This is my first web page using PHP.</p>'; ?>
+    <?php echo '<h4>All content on this page was displayed using php</h4>'; ?>
+  </body>
+</html>
